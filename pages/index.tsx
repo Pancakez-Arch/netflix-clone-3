a7503954f2d4b07ae9f52d38c38ff9f84a4921b4
Ad-#1-am <thedarkgamer256@gmail.com>
2024-11-20 05:58:22 +0100

    OAuth and secure something
    Added Google and Github OAuth and made it so people HAVE to log in to actually use the website

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,38 @@
-const Home = () => {
+import useCurrentUser from "@/hooks/useCurrentUser";
+import { NextPageContext } from "next";
+import { getSession, signOut } from "next-auth/react"; 
+
+export async function getServerSideProps(context: NextPageContext) {
+  const session = await getSession(context);
+
+  if (!session) {
+    return {
+      redirect: {
+        destination: '/auth',
+        permanent: false,
+      }
+    }
+  }
+
+  return {
+    props: {},
+  }
+}
+
+export default function Home() {
+  const { data: user } = useCurrentUser();
+
+  
   return (
-    <div>
-      <h1 className="text-3xl text-green-300">Home</h1>
-    </div>
+    <>
+      <h1 className="text-7xl text-green-300">Home</h1>
+      <p className="text-white"> Logged in as: {user?.email}</p>
+      <button className="h-10 w-full bg-white" onClick={() => signOut()}>Logout</button>
+    </>
   )
 }
 
-export default Home;
+
+  
+
+
